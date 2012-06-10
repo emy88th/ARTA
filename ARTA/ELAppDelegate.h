8a9949ad513ae29d641ed82c3d6e399ef5fde543
Emanuel Lupu-Marinei <emy_88th@yahoo.com>
2012-06-10 12:09:23 +0300

    - added app skeleton

---
+++ ARTA/ELAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ELAppDelegate.h
+//  ARTA
+//
+//  Created by Lupu Emanuel on 6/9/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ELAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
